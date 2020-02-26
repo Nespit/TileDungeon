987057e574c7fde8cee39fe36f046672150a9bec
Leonhard Pickny <leonhard.pickny@gmail.com>
2020-02-26 21:08:39 +0100

    Tile based movement is working, goals for next iteration are in the CharacterMovement script.

---
+++ TileDungeon/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    //Doors blocking without key, fix blocking issue, key, coins, display points
+    
+    Vector3 currentDirection;
+    Vector3 moveDestination, nextPos;
+    Quaternion targetRotation;
+
+    public float movementSpeed = 2f;
+    public float rotationSpeed = 5f;
+
+    bool move = false;
+
+
+    void Start()
+    {
+        currentDirection = transform.position;
+        targetRotation = transform.rotation;
+    }
+    Ray myRay;
+    void Update()
+    {
+        CharacterInput();
+        Movement();
+    
+        Debug.DrawRay(myRay.origin, myRay.direction, Color.red);
+
+    }
+
+    void MoveTowardsDestination(Vector3 destination)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, destination, movementSpeed*Time.deltaTime);
+    }
+
+    void SetRotationTowardsTarget(Vector3 target)
+    {
+        Vector3 direction = (target - transform.position).normalized;
+        targetRotation = Quaternion.LookRotation(direction);
+    }
+    void LookTowards(Quaternion rotation)
+    {
+       transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+    }
+
+    public void Movement()
+    {
+        if(!move)
+        {
+            return;
+        }
+        
+        if(transform.rotation != targetRotation)
+        {
+            LookTowards(targetRotation);
+        }
+            
+        else if (transform.position != moveDestination)
+        {
+            MoveTowardsDestination(moveDestination);
+        } 
+        
+        else
+        move = false;
+    }
+
+    bool AttemptMove(Vector3 target)
+    {
+        myRay = new Ray(target, new Vector3(0,-1,0));
+        RaycastHit hit;
+
+        if(Physics.Raycast(myRay, out hit))
+        {
+            if(hit.collider.tag == "Tile")
+            return true;
+        }
+        return false;
+    }
+
+    void CharacterInput()
+    {
+        if(move)
+        {
+            return;
+        }
+
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 target = transform.position+transform.forward; 
+            
+            if(AttemptMove(target))
+            {
+                moveDestination = target;
+                SetRotationTowardsTarget(target);
+                move = true;
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector3 target = transform.position-transform.forward; 
+            
+            if(AttemptMove(target))
+            {
+                moveDestination = target;
+                SetRotationTowardsTarget(target);
+                move = true;
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Vector3 target = transform.position+transform.right; 
+
+            if(AttemptMove(target))
+            {
+                moveDestination = target;
+                SetRotationTowardsTarget(target);
+                move = true;
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Vector3 target = transform.position-transform.right; 
+
+            if(AttemptMove(target))
+            {
+                moveDestination = target;
+                SetRotationTowardsTarget(target);
+                move = true;
+            }
+        }
+    }
+}
--- TileDungeon/Assets/Scripts/GridManager.cs
+++ TileDungeon/Assets/Scripts/GridManager.cs
@@ -1,18 +1,114 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
 
+[ExecuteInEditMode]
 public class GridManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    public GameObject tilePrefab;
+    [SerializeField]
+    public Vector2 maxGridSize;
+
+    Grid grid;
+
+    void Awake()
     {
-        
+        if (transform.childCount > 0)
+        {
+            grid = ConvertSceneToGrid();
+        }
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        GridOutline();
+    }
+    
+    private void GridOutline()
+    {
+        //Vertices & UVs
+        Vector3[] vertices = 
+        {
+            new Vector3 ((float)-0.5, 0, (float)-0.5),
+            new Vector3 ((float)(maxGridSize.x-0.5), 0, (float)-0.5),
+            new Vector3 ((float)(maxGridSize.x-0.5), (float)0.01, (float)-0.5),
+            new Vector3 ((float)-0.5, (float)0.01, (float)-0.5),
+            new Vector3 ((float)-0.5, (float)0.01, (float)(maxGridSize.y-0.5)),
+            new Vector3 ((float)(maxGridSize.x-0.5), (float)0.01, (float)(maxGridSize.y-0.5)),
+            new Vector3 ((float)(maxGridSize.x-0.5), 0, (float)(maxGridSize.y-0.5)),
+            new Vector3 ((float)-0.5, 0, (float)(maxGridSize.y-0.5))
+        };
+
+        Vector2[] uv = 
+        {
+            new Vector2 (0, 0),
+            new Vector2 (1, 0),
+            new Vector2 (1, 1),
+            new Vector2 (0, 1),
+            new Vector2 (0, 0),
+            new Vector2 (1, 0),
+            new Vector2 (1, 1),
+            new Vector2 (0, 1)
+        };
+
+        //Triangles
+        int[] triangleIndices = 
+        {
+            0, 2, 1, //face front
+            0, 3, 2,
+            2, 3, 4, //face top
+            2, 4, 5,
+            1, 2, 5, //face right
+            1, 5, 6,
+            0, 7, 4, //face left
+            0, 4, 3,
+            5, 4, 7, //face back
+            5, 7, 6,
+            0, 6, 7, //face bottom
+            0, 1, 6
+        };
+
+        Mesh mesh = new Mesh
+        {
+            vertices = vertices,
+            uv = uv,
+            triangles = triangleIndices
+        };
+
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+
+        meshFilter.mesh = mesh;
+    }
+
+    Grid ConvertSceneToGrid()
+    {
+        int tileCount = transform.childCount;
+        Grid grid;
+        grid.tiles = new Tile[tileCount, tileCount];
+        
+        foreach(Transform t in transform)
+        {
+             grid.tiles[Mathf.RoundToInt(t.position.x), Mathf.RoundToInt(t.position.z)].gameObject = t.gameObject;
+        }
+
+        return  grid;
+    }
+
     void Update()
     {
-        
+
     }
+}
+
+public struct Tile
+{
+    public GameObject gameObject;
+}
+
+public struct Grid
+{
+    public Tile[,] tiles;
 }
